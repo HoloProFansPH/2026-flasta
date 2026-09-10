001866659c9dc348e9e9438fe2521d8b90ae7ef3
Madekuji <27665110+Madekuji@users.noreply.github.com>
2026-09-11 02:09:09 +0800

    update conversion date

--- src/components/GoalTracker.svelte
+++ src/components/GoalTracker.svelte
@@ -112,7 +112,7 @@
     <p>
         For international transactions, the exchange rate is calculated as USD 1
         = PHP 57.20, which is derived from the conversion rate provided by
-        PayPal (Kofi's payment processor) on September , 2026, in addition to
+        PayPal (Kofi's payment processor) on September 6, 2026, in addition to
         other conversion and international transfer fees issued by PayPal. The
         actual, current exchange rate may differ from the rate used for this
         tracker.
